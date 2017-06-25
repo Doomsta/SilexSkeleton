8a76dd91bc3eef813777df0e21f7f1f171371520
arandel <andre.randel@mailbox.org>
2017-06-25 16:39:56 +0200

    stuff

--- src/Bootstrapper.php
+++ src/Bootstrapper.php
@@ -1,12 +1,48 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: arandel
- * Date: 25.06.17
- * Time: 09:52
- */
 class Bootstrapper
 {
+    private const DS = DIRECTORY_SEPARATOR;
+    private static $isBootstrapped = false;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return bool
+     */
+    public function bootstrap(): bool
+    {
+        if (self::$isBootstrapped) {
+            return false;
+        }
+        define('ROOT_PATH', realpath(__DIR__ . self::DS . '..'));
+        self::$isBootstrapped = true;
+
+        return true;
+    }
+
+    public function isBootstrapped(): bool
+    {
+        return self::$isBootstrapped;
+    }
+
+    /**
+     * @var \App\Application|null
+     */
+    private static $app;
+
+    /**
+     * @return \App\Application
+     */
+    public static function getApp(): \App\Application
+    {
+        self::bootstrap();
+        if (self::$app === null) {
+            self::$app = new \App\Application();
+        }
+        return self::$app;
+    }
 
 }
